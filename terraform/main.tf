d0d30e6e78548e24b48018af17455af1fbb19dbf
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-11-16 19:55:47 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
     region = var.region
 }
 module  "vpc" {
-    vpc_name = var.vpc_name
+    vpc_name = "movies-bot-vpc"
     source = "./modules/vpc"
     cidr_block           = "10.0.0.0/16"
     enable_dns_support   = true
     enable_dns_hostnames = true
 
     tags = {
-        vpc_name = var.vpc_name
+        vpc_name =  "movies-bot-vpc"
     }
 }
