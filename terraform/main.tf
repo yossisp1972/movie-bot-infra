6a30b86ea46bb8e77e74c31c0cceb10c4576c433
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-11-17 08:35:53 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,14 +2,22 @@ provider "aws" {
     region = var.region
 }
 module  "vpc" {
-    source = "./modules/vpc"
-    vpc_name = var.vpc_name
-    cidr_block           = "10.0.0.0/16"
-    enable_dns_support   = true
-    enable_dns_hostnames = true
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
 
-    tags = {
-        Name =  var.vpc_name
-    }
+  name = var.vpc_name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "dev"
+  }
     
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,4 +5,12 @@ terraform {
             version = ">= 5.0"
         }
     }
+
+    backend "s3" {
+        bucket         = "movie-bot-tfstate"
+        key            = "global/s3/terraform.tfstate"
+        region         = "us-east-1"
+        dynamodb_table = "movie-bot-tflock" # optional, for state locking
+        encrypt        = true
+  }
 }
