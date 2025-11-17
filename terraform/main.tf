2c17459007033cb66d9527573ec630ff5e385e94
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-11-17 11:27:05 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,26 +24,47 @@ module  "vpc" {
 }
 
 module "eks" {
-    source = "terraform-aws-modules/eks/aws"
-    version = "18.0.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
 
-    cluster_name    = var.cluster_name
-    cluster_version = var.cluster_version
-    subnet_ids      = module.vpc.private_subnets
-    vpc_id          = module.vpc.vpc_id
+  name               = var.cluster_name
+  kubernetes_version = "1.33"
 
-    eks_managed_node_groups = {
-        eks_nodes = {
-            desired_capacity = 2
-            max_capacity     = 3
-            min_capacity     = 1
-
-            instance_type = "t1.small"
-            key_name      = var.key_name
-
-            tags = {
-                Name = "eks-node"
-            }
-        }
+  addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {
+      before_compute = true
     }
+    kube-proxy             = {}
+    vpc-cni                = {
+      before_compute = true
+    }
+  }
+
+  # Optional
+  endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = module.vpc.id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    workers = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.small"]
+
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
