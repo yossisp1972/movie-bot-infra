f8a104a03b2bd4c324a457745dd0da8e5ba19cbe
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-11-19 11:35:31 +0200

    test

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -8,7 +8,15 @@ terraform {
   #     name = "learn-terraform-eks"
   #   }
   # }
-
+  backend "s3" {
+        bucket         = "movie-bot-tfstate"
+        key            = "global/s3/terraform.tfstate"
+        region         = "us-east-1"
+        use_lockfile   = true
+        dynamodb_table = "movie-bot-tflock" # optional, for state locking
+        encrypt        = true
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
