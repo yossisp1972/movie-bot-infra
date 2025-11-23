fa225255bc64d7c5d83b287e6418a1ff34b44d3d
Yossi-Spindler_verint <yossi.spindler@verint.com>
2025-11-23 15:58:00 +0200

    test

--- tf/main.tf
+++ tf/main.tf
@@ -74,9 +74,10 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-  # Disable NAT Gateway - use VPC Endpoints instead for cost savings
-  enable_nat_gateway   = false
-  single_nat_gateway   = false
+  # Enable NAT Gateway for internet access (Helm needs to pull from public registries)
+  # VPC Endpoints handle AWS service traffic, NAT handles internet traffic
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   
   enable_dns_hostnames = true
   enable_dns_support   = true
